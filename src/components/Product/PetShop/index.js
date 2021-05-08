4a8a2381b9fb05c46e5d7c15ee2fbe20c4e65fd1
Paulo Henrique <phvcandido@gmail.com>
2021-05-08 19:05:35 -0300

    Alterando estilos da página de produto

--- src/components/Product/PetShop/index.js
+++ src/components/Product/PetShop/index.js
@@ -2,7 +2,7 @@ import "./style.scss";
 
 function Product() {
   return (
-    <div className="col-md-3 mb-5">
+    <div className="col-md-3 mb-5 petshopProduct">
       <div className="containProductImage d-flex pt-4 justify-content-center position-relative">
         <img
           src="https://images-na.ssl-images-amazon.com/images/I/715aYupxI5L._AC_SX466_.jpg"
@@ -13,10 +13,6 @@ function Product() {
         </button>
       </div>
       <div className="d-flex justify-content-between w-100 pt-2">
-        <div className="left">
-          <span className="mdi mdi-star"></span>
-          <b>2,8</b>
-        </div>
         <label className="badge">R$ 22,90</label>
       </div>
       <div className="title pt-2">
