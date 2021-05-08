904f92998f284505f585cf8af761006ca48fc697
Paulo Henrique <phvcandido@gmail.com>
2021-05-08 20:43:18 -0300

    Ajustando posicionamento do header de produtos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,7 +7,7 @@ function Home() {
   return (
     <div className="">
       <Header hideCart />
-      <div className="container-fluid petshop-list-container">
+      <div className="petshop-list-container">
         <div className="col-12 px-4 text-center">
           <h5 className="mb-5">Mais próximos de você (5)</h5>
         </div>
