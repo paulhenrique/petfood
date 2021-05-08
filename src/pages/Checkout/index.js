14a2a3c3697575a22baf240f8f31e5162ff235fe
Paulo Henrique <phvcandido@gmail.com>
2021-05-08 18:10:18 -0300

    Adicionando estilo da página Checkout

---
+++ src/pages/Checkout/index.js
@@ -0,0 +1,129 @@
+import Header from "../../components/Header";
+import Product from "../../components/Product/list";
+import "./style.scss";
+function Checkout() {
+  return (
+    <div className="h-100">
+      <Header />
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-6">
+            <span className="section-title">Dados de Entrega</span>
+            <div className="row mb-3">
+              <div className="col-12">
+                <input
+                  type="text"
+                  placeholder="CEP"
+                  className="form-control form-control-lg"
+                />
+              </div>
+            </div>
+            <div className="row mb-3">
+              <div className="col-6">
+                <input
+                  type="text"
+                  placeholder="Cidade"
+                  className="form-control form-control-lg"
+                />
+              </div>
+              <div className="col-6">
+                <input
+                  type="text"
+                  placeholder="Logradouro"
+                  className="form-control form-control-lg"
+                />
+              </div>
+            </div>
+            <div className="row mb-3">
+              <div className="col-5">
+                <input
+                  type="text"
+                  placeholder="Número"
+                  className="form-control form-control-lg"
+                />
+              </div>
+              <div className="col-5">
+                <input
+                  type="text"
+                  placeholder="Bairro"
+                  className="form-control form-control-lg"
+                />
+              </div>
+              <div className="col-2">
+                <input
+                  type="text"
+                  placeholder="UF"
+                  className="form-control form-control-lg"
+                />
+              </div>
+            </div>
+            <div className="row mb-3">
+              <div className="col-6">
+                <input
+                  type="text"
+                  placeholder="Bairro"
+                  className="form-control form-control-lg"
+                />
+              </div>
+              <div className="col-6">
+                <input
+                  type="text"
+                  placeholder="Complemento"
+                  className="form-control form-control-lg"
+                />
+              </div>
+            </div>
+
+            <span className="section-title">Dados de Pagamento</span>
+            <div className="row mb-3">
+              <div className="col-12">
+                <input
+                  type="text"
+                  placeholder="Número do Cartão"
+                  className="form-control form-control-lg"
+                />
+              </div>
+            </div>
+            <div className="row mb-3">
+              <div className="col-6">
+                <input
+                  type="text"
+                  placeholder="Validade"
+                  className="form-control form-control-lg"
+                />
+              </div>
+              <div className="col-6">
+                <input
+                  type="text"
+                  placeholder="CVV"
+                  className="form-control form-control-lg"
+                />
+              </div>
+            </div>
+            <div className="row mt-4">
+              <div className="col-12  d-flex justify-content-between align-items-center">
+                <b>Total</b>
+                <h3>R$ 30,00</h3>
+              </div>
+              <div className="col-12">
+                <div className="btn btn-block w-100 btn-lg btn-primary">
+                  Finalizar Compra
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="box mb-4">
+              <h4>Minha Sacola (5)</h4>
+              <div className="row products">
+                <Product />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
