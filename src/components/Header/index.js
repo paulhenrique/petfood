1f3b414a32ad06a83208077b25c378341190d791
Paulo Henrique <phvcandido@gmail.com>
2021-05-08 19:12:19 -0300

    Alterações no header
    Adicionando botão do carrinho

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,27 @@
 import Logo from "../../assets/logo.png";
-import LogoWhite from '../../assets/logo-white.png';
+import LogoWhite from "../../assets/logo-white.png";
 function Header({ whiteVersion }) {
   return (
-    <div className="col-12 text-center py-5">
-      <header>
-        <img src={whiteVersion ? LogoWhite : Logo} alt="PetFood" className="img-responsive" />
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-6 text-center py-5 offset-3 d-flex justify-content-center align-items-center">
+          <header>
+            <img
+              src={whiteVersion ? LogoWhite : Logo}
+              alt="PetFood"
+              className="img-responsive"
+            />
+          </header>
+        </div>
+        <div className="col-3 text-align-right d-flex justify-content-end align-items-center">
+          <button class="btn btn-secondary ml-0">
+            <span className="mdi mdi-cart pr-1"></span>
+            <b>2 Itens</b>
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
