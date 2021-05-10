22be4b4d91f48582b6dcf522afef77d590d37c82
Paulo Henrique <phvcandido@gmail.com>
2021-05-10 20:10:17 -0300

    Adicionando redux

---
+++ src/store/modules/shop/reducer.js
@@ -0,0 +1,14 @@
+const INITIAL_STATE = {};
+
+function shop(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case 'SET_CUSTOMER': {
+      //LÓGICA
+      break;
+
+    }
+    default: {
+      return state;
+    }
+  }
+}
